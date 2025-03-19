15def6994c3d59dcb813b36a10f9a69de34cc20a
Natnael Yirga <Natnael.Yirga@appdiv.com>
2025-03-19 12:04:41 +0300

    fix: revert version to 0.0.1 in package.json and update tailwind.config.js content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: [
+    "./src/**/*.{html,ts}",
+    // "./projects/lucy-calendar/src/**/*.{html,ts}" // Add this line
+  ],
   theme: {
     extend: {},
   },
