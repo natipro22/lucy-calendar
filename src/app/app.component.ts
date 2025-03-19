0201a5472dc267a3a85d61cb325e3119a8a00318
Natnael Yirga <Natnael.Yirga@appdiv.com>
2025-03-19 14:34:44 +0300

    feat: implement Ethiopian date conversion in LucyCalendar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LucyCalendarComponent } from "../../projects/lucy-calendar/src/lib/lucy-calendar.component";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { dayNames, monthNames, toEthiopian } from '../../projects/lucy-calendar/src/lib/date-convertor';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LucyCalendarComponent],
+  imports: [RouterOutlet, LucyCalendarComponent, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'myapp';
-  date: string = '';
-  selectedYear: number = 2017;
-  today: Date = new Date();
-  min: Date = new Date(2025, 0, 1);
+  date: Date | null = null;
+  etDate: string = '';
+  grDate: string = '';
   availableYears: number[] = Array.from({ length: 101 }, (_, i) => new Date().getFullYear() - 50 + i);
-  eventDate: string = '';
-  birthDate: string = '';
+
+  onDateChange(value: string | Date | null) {
+    if (typeof value === 'string') {
+      this.date = new Date(value);
+    }
+    else if (value instanceof Date) {
+      this.date = value;
+    }
+    if (!this.date) {
+      return;
+    }
+    const dateEt = toEthiopian(this.date); // Call the conversion function
+    this.etDate = `${dayNames[this.date.getDay()]}, ${monthNames[dateEt.month - 1]} ${this.padZero(dateEt.day)} ${dateEt.year}`;
+  }
+
+  padZero(num: number): string {
+    return num.toString().padStart(2, '0');
+  }
 
 }
