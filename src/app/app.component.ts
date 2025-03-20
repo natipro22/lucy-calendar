cd8d3e6d6d8f6e629b6a395bb585163c277cfc61
Natnael Yirga <Natnael.Yirga@appdiv.com>
2025-03-20 10:59:13 +0300

    fix: remove unused title property and update index.html title to 'Lucy Calendar Date Picker'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,6 @@ import { dayNames, monthNames, toEthiopian } from '../../projects/lucy-calendar/
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Lucy Calendar Date Picker';
   date: Date | null = null;
   lucyDate: string | null = null;
   etDate: string = '';
